8e3e0dfa4f48ef21dd1f8ee2ea6ed2cb33b4b449
Ricardo Miranda <rmiranda@teco.com.ar>
2024-07-16 13:00:31 -0300

    Complex and Vector tests

---
+++ tests/complex.test.js
@@ -0,0 +1,84 @@
+import { describe, expect, it } from 'vitest';
+import { Complex } from '../src';
+describe(Complex.name, () => {
+    it('creates a new complex instance', () => {
+        const cpx = new Complex(0, 1);
+        expect(cpx).toBeInstanceOf(Complex);
+        expect(cpx.a).toBe(0);
+        expect(cpx.b).toBe(1);
+    });
+    const magnitudes = [
+        [1, 0, 1],
+        [-1, 0, 1],
+        [0, 1, 1],
+        [0, -1, 1],
+        [0, 0, 0],
+        [1, -1, Math.SQRT2],
+        [5, 12, 13],
+    ];
+    it.each(magnitudes)('calculates the magnitude correctly', (a, b, mag) => {
+        const cpx = new Complex(a, b);
+        expect(cpx.mag).toBeCloseTo(mag, 6);
+    });
+    it('adds a real number', () => {
+        let cpx = new Complex(8, 5);
+        cpx = cpx.add(22);
+        expect(cpx.a).toBe(30);
+        expect(cpx.b).toBe(5);
+    });
+    it('adds a complex number', () => {
+        let cpx1 = new Complex(8, 5);
+        let cpx2 = new Complex(-5, -8);
+        const cpx = cpx1.add(cpx2);
+        expect(cpx.a).toBe(3);
+        expect(cpx.b).toBe(-3);
+    });
+    it('subs a real number', () => {
+        let cpx = new Complex(-7, 2);
+        cpx = cpx.sub(-8);
+        expect(cpx.a).toBe(1);
+        expect(cpx.b).toBe(2);
+    });
+    it('subs a complex number', () => {
+        let cpx1 = new Complex(8, 5);
+        let cpx2 = new Complex(-5, -8);
+        const cpx = cpx1.sub(cpx2);
+        expect(cpx.a).toBe(13);
+        expect(cpx.b).toBe(13);
+    });
+    it('multiplies by real number', () => {
+        let cpx = new Complex(8, 5);
+        cpx = cpx.mult(2);
+        expect(cpx.a).toBe(16);
+        expect(cpx.b).toBe(10);
+    });
+    it('multiplies by complex number', () => {
+        let cpx1 = new Complex(2, 1);
+        let cpx2 = new Complex(-1, -1);
+        const cpx = cpx1.mult(cpx2);
+        expect(cpx.a).toBe(-1);
+        expect(cpx.b).toBe(-3);
+    });
+    it('divides by real number', () => {
+        let cpx = new Complex(8, 5);
+        cpx = cpx.div(2);
+        expect(cpx.a).toBe(4);
+        expect(cpx.b).toBe(2.5);
+    });
+    it('divides by complex number', () => {
+        let cpx1 = new Complex(2, 1);
+        let cpx2 = new Complex(-1, -1);
+        const cpx = cpx1.div(cpx2);
+        expect(cpx.a).toBe(-1.5);
+        expect(cpx.b).toBe(0.5);
+    });
+    it('gets the square of a complex number', () => {
+        let cpx = new Complex(4, 0);
+        cpx = cpx.sqrt();
+        expect(cpx.a).toBe(2);
+        expect(cpx.b).toBe(0);
+        cpx = new Complex(3, 4);
+        cpx = cpx.sqrt();
+        expect(cpx.mag).toBe(Math.sqrt(5));
+    });
+});
---
+++ tests/vector.test.js
@@ -0,0 +1,106 @@
+import { describe, expect, it } from "vitest";
+import { Vector } from "../src";
+describe(Vector.name, () => {
+    it('creates new instance', () => {
+        const v = new Vector(0, 1);
+        expect(v).toBeInstanceOf(Vector);
+    });
+    it('calculates magnitude', () => {
+        const v = new Vector(0, 1);
+        expect(v.mag).toBe(1);
+        v.x = 1;
+        expect(v.mag).toBe(Math.SQRT2);
+    });
+    it('calculates angle', () => {
+        const v = new Vector(0, 1);
+        expect(v.angle).toBe(Math.PI * 0.5);
+        v.x = 1;
+        expect(v.angle).toBe(Math.PI * 0.25);
+    });
+    it('calculates angle on vector zero', () => {
+        const v = new Vector(0, 0);
+        expect(v.angle).toBe(0);
+    });
+    it('normalizes vector', () => {
+        const v = new Vector(0, 3);
+        v.normalize();
+        expect(v.mag).toBe(1);
+        expect(v.y).toBe(1);
+        v.x = 5;
+        v.y = 5;
+        v.normalize();
+        expect(v.mag).toBeCloseTo(1, 6);
+        expect(v.x).toBeCloseTo(Math.SQRT1_2, 6);
+        expect(v.y).toBeCloseTo(Math.SQRT1_2, 6);
+    });
+    it('does not normalizes vector zero', () => {
+        const v = new Vector(0, 0);
+        v.normalize();
+        expect(v.mag).toBe(0);
+        expect(v.x).toBe(0);
+        expect(v.y).toBe(0);
+    });
+    it('multiplies a vector', () => {
+        const v = new Vector(0, 3);
+        v.mult(4);
+        expect(v.mag).toBe(12);
+        expect(v.y).toBe(12);
+        v.x = 5;
+        v.y = 5;
+        v.mult(2);
+        expect(v.mag).toBeCloseTo(10 * Math.SQRT2, 6);
+        expect(v.x).toBe(10);
+        expect(v.y).toBe(10);
+    });
+    it('adds a vector', () => {
+        const v = new Vector(-2, 3);
+        v.add(new Vector(3, -5));
+        expect(v.x).toBe(1);
+        expect(v.y).toBe(-2);
+    });
+    it('subs a vector', () => {
+        const v = new Vector(-2, 3);
+        v.sub(new Vector(3, -5));
+        expect(v.x).toBe(-5);
+        expect(v.y).toBe(8);
+    });
+    it('calculates distance against another vector', () => {
+        const v = new Vector(7, 2);
+        const d = v.dist(new Vector(3, -1));
+        expect(d).toBe(5);
+    });
+    it('verifies equality with another vector', () => {
+        const v1 = new Vector(7, 2);
+        const v2 = new Vector(7, 2);
+        expect(v1.equals(v2)).toBeTruthy();
+        v2.x = 9;
+        expect(v1.equals(v2)).toBeFalsy();
+    });
+    it('calculates angle against another vector', () => {
+        const v1 = new Vector(0, 1);
+        const v2 = new Vector(1, 0);
+        expect(v1.angleTo(v2)).toBeCloseTo(Math.PI * 0.5, 6);
+        v2.y = 1;
+        expect(v1.angleTo(v2)).toBeCloseTo(Math.PI * 0.25, 6);
+    });
+    it('copies a vector', () => {
+        const v = new Vector(7, 2);
+        const cv = v.copy();
+        expect(v.equals(cv)).toBeTruthy();
+        expect(cv).not.toBe(v);
+    });
+    it('trasposes a vector', () => {
+        const v = new Vector(7, 2);
+        v.transpose();
+        expect(v.x).toBe(2);
+        expect(v.y).toBe(7);
+    });
+    it('creates vector from angle', () => {
+        let v = Vector.fromAngle(Math.PI * 0.5);
+        expect(v.x).toBeCloseTo(0, 6);
+        expect(v.y).toBeCloseTo(1, 6);
+        v = Vector.fromAngle(Math.PI * 0.75);
+        expect(v.x).toBeCloseTo(-Math.SQRT1_2, 6);
+        expect(v.y).toBeCloseTo(Math.SQRT1_2, 6);
+    });
+});
