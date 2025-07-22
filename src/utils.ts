06aad5394fd09ad96869d100b61b428c1d3e6846
Ricardo Miranda <rmiranda@teco.com.ar>
2025-07-22 11:05:19 -0300

    Orientation util

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { Vector } from "./vector";
+
 /**
  * Generates an array of numbers from a `start` (inclusively) to an `end`
  * (exclusively), using a `step`
@@ -31,4 +33,24 @@ export function range(start: number, end: number, step: number = 1): number[] {
     }
   }
   return a;
+}
+
+/**
+ * To find orientation of ordered triplet (p, q, r).
+ * The function returns following values
+ * 0 when p, q and r are collinear; -1 when clockwise, 1 counterclockwise
+ *
+ * @param { Vector } p
+ * @param { Vector } q
+ * @param { Vector } r
+ * @returns { number }
+ */
+export function getOrientation(p: Vector, q: Vector, r: Vector): number {
+  // See https://www.geeksforgeeks.org/orientation-3-ordered-points/
+  // for details of below formula.
+  let val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
+
+  if (val == 0) return 0; // collinear
+
+  return val > 0 ? -1 : 1; // clock or counterclock wise
 }
