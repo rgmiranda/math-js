7c9f69e53a1d6da8651b07828961a3719dabd0d8
Ricardo Miranda <rmiranda@teco.com.ar>
2025-07-22 11:30:22 -0300

    New segments definitions

--- src/segment.ts
+++ src/segment.ts
@@ -1,83 +1,87 @@
+import { Line } from "./line";
+import { getOrientation } from "./utils";
 import { Vector } from "./vector";
 
 export class Segment {
-    constructor (public readonly p: Vector, public readonly q: Vector) {
+  public readonly length: number;
+  public readonly midpoint: Vector;
+  public readonly direction: Vector;
+
+  constructor(public readonly start: Vector, public readonly end: Vector) {
+    this.length = Vector.sub(start, end).mag;
+    this.midpoint = start.copy().add(end).mult(0.5);
+    this.direction = end.copy().sub(start).normalize();
+  }
+
+  /**
+   *
+   * @param { Vector } r
+   * @param { number } tolerance
+   * @returns { boolean }
+   */
+  contains(r: Vector, tolerance: number = 1e-10): boolean {
+    const cross =
+      (this.end.x - this.start.x) * (r.y - this.start.y) -
+      (this.end.y - this.start.y) * (r.x - this.start.x);
+    if (Math.abs(cross) > tolerance) {
+      return false;
     }
 
-    /**
-     * 
-     * @param { Vector } r 
-     * @returns { boolean }
-     */
-    isOnSegment(r: Vector): boolean {
-        return (this.q.x - this.p.x)*(r.y - this.p.y) === (this.q.y - this.p.y) * (r.x - this.p.x);
+    const withinX =
+      r.x <= Math.max(this.start.x, this.end.x) + tolerance &&
+      r.x >= Math.min(this.start.x, this.end.x) - tolerance;
+    const withinY =
+      r.y <= Math.max(this.start.y, this.end.y) + tolerance &&
+      r.y >= Math.min(this.start.y, this.end.y) - tolerance;
+
+    return withinX && withinY;
+  }
+
+  /**
+   *
+   * Returns true if intersects with this segment
+   *
+   * @param { Segment } segment
+   * @returns { boolean }
+   */
+  intersects(segment: Segment): boolean {
+    // Find the four orientations needed for general and
+    // special cases
+    let o1 = getOrientation(this.start, this.end, segment.start);
+    let o2 = getOrientation(this.start, this.end, segment.end);
+    let o3 = getOrientation(segment.start, segment.end, this.start);
+    let o4 = getOrientation(segment.start, segment.end, this.end);
+
+    // General case
+    if (o1 != o2 && o3 != o4) {
+      return true;
     }
 
-    /**
-     * To find orientation of ordered triplet (p, q, r).
-     * The function returns following values
-     * 0 when p, q and r are collinear; -1 when clockwise, 1 counterclockwise
-     * 
-     * @param { Vector } p 
-     * @param { Vector } q 
-     * @param { Vector } r 
-     * @returns { number }
-     */
-    private getOrientation(p: Vector, q: Vector, r: Vector): number {
-
-        // See https://www.geeksforgeeks.org/orientation-3-ordered-points/ 
-        // for details of below formula. 
-        let val = (q.y - p.y) * (r.x - q.x) -
-            (q.x - p.x) * (r.y - q.y);
-
-        if (val == 0) return 0; // collinear 
-
-        return (val > 0) ? -1 : 1; // clock or counterclock wise 
+    // Special Cases
+    // The first point of the segemnt lies on this segment
+    if (o1 == 0 && this.contains(segment.start)) {
+      return true;
     }
 
-    
-    /**
-     * 
-     * Returns true if intersects with this segment
-     * 
-     * @param { Segment } segment
-     * @returns { boolean }
-     */
-   intersects(segment: Segment): boolean {
-
-        // Find the four orientations needed for general and 
-        // special cases 
-        let o1 = this.getOrientation(this.p, this.q, segment.p);
-        let o2 = this.getOrientation(this.p, this.q, segment.q);
-        let o3 = this.getOrientation(segment.p, segment.q, this.p);
-        let o4 = this.getOrientation(segment.p, segment.q, this.q);
-
-        // General case 
-        if (o1 != o2 && o3 != o4) {
-            return true;
-        }
-
-        // Special Cases 
-        // The first point of the segemnt lies on this segment 
-        if (o1 == 0 && this.isOnSegment(segment.p)) {
-            return true;
-        }
-
-        // The second point of the segemnt lies on this segment 
-        if (o2 == 0 && this.isOnSegment(segment.q)) {
-            return true;
-        }
-
-        // The first point of this segments lies on the other segment 
-        if (o3 == 0 && segment.isOnSegment(this.p)) {
-            return true;
-        }
-
-        // The second point of this segments lies on the other segment 
-        if (o4 == 0 && segment.isOnSegment(this.q)) {
-            return true;
-        }
-
-        return false; // Doesn't fall in any of the above cases 
+    // The second point of the segemnt lies on this segment
+    if (o2 == 0 && this.contains(segment.end)) {
+      return true;
     }
-};
+
+    // The first point of this segments lies on the other segment
+    if (o3 == 0 && segment.contains(this.start)) {
+      return true;
+    }
+
+    // The second point of this segments lies on the other segment
+    if (o4 == 0 && segment.contains(this.end)) {
+      return true;
+    }
+
+    return false; // Doesn't fall in any of the above cases
+  }
+
+  toLine(): Line {
+    return Line.fromPoints(this.start, this.end);
+  }
+}
